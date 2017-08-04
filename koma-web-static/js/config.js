03b78fb1cabffadde04ca1dac54c2f1b79331c67
dado <littlevwing@live.com>
2017-08-04 13:46:10 +0200

    copied statis web site

---
+++ koma-web-static/js/config.js
@@ -0,0 +1,8 @@
+var configConstants = {
+	auth0: {
+		domain: 'littlevwing.auth0.com',
+		clientId: 'j54pWi8fcQPC6eLy02v8jsIF_x9PgucP'
+	},
+  apiBaseUrl: 'https://qaov2snb71.execute-api.us-west-2.amazonaws.com/dev'
+};
+
