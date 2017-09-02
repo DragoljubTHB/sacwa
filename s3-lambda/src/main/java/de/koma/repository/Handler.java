68f923e4977e386f6e9cd655caffec41e0de04e3
dado <littlevwing@live.com>
2017-09-02 23:08:26 +0200

    loaded s3 with java tough config

--- s3-lambda/src/main/java/de/koma/repository/Handler.java
+++ s3-lambda/src/main/java/de/koma/repository/Handler.java
@@ -1,7 +1,10 @@
 package de.koma.repository;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
@@ -9,14 +12,28 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
 import java.io.*;
 import java.util.StringJoiner;
 
+/**
+ * Config:: 512MB x 10''
+ * first run ~ 7''
+ * secon run ~ .3''
+ */
 public class Handler implements RequestHandler<Object, String> {
-
+    String baseUrl = "https://s3.amazonaws.com";
     @Override
     public String handleRequest(Object input, Context context) {
-        context.getLogger().log("start");
         context.getLogger().log("intput: "+input);
-        S3Object s3Object = AmazonS3ClientBuilder.standard()
-                .build().getObject("ontology.thb.de", "koma-complex.owl");
+        context.getLogger().log("env: "+input);
+        context.getClientContext().getEnvironment().get("BUCKET");
+        AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard()
+                .withRegion(Regions.US_WEST_2)
+                .build();
+        context.getLogger().log("builded "
+                +amazonS3.doesBucketExist("ontology.thb.de"));
+
+        S3Object s3Object = amazonS3
+                .getObject("ontology.thb.de", "koma-complex.owl");
+
+        context.getLogger().log("loaded object");
 
         try(BufferedReader br = new BufferedReader(
                 new InputStreamReader(
