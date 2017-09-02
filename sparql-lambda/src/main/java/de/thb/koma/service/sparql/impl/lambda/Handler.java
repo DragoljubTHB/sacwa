f65e555ecae7d5631a47ad92c20f99ac0ccba3f7
dado <littlevwing@live.com>
2017-09-02 23:34:18 +0200

    decopling config from code

--- sparql-lambda/src/main/java/de/thb/koma/service/sparql/impl/lambda/Handler.java
+++ sparql-lambda/src/main/java/de/thb/koma/service/sparql/impl/lambda/Handler.java
@@ -1,5 +1,6 @@
 package de.thb.koma.service.sparql.impl.lambda;
 
+import com.amazonaws.regions.Regions;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -24,21 +25,24 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.PrintWriter;
 
-public class Handler implements RequestHandler<Object, String> {
+public class Handler implements RequestHandler<OntologyKey, String> {
+    String expectedEnv = "BUCKET";
 
     @Override
-    public String handleRequest(Object input, Context context) {
+    public String handleRequest(OntologyKey input, Context context) {
         LambdaLogger logger = context.getLogger();
-        logger.log("handleRequest "+input);
-
-        S3Object s3Object = AmazonS3ClientBuilder.defaultClient()
-                .getObject("ontology.koma.de", "koma-complex.owl");
+        logger.log("handleRequest " + input);
 
         FusekiController ctrl = new FusekiController();
         ctrl.initServer();
         logger.log("server init");
 
-        ctrl.uploadModelToServer(ctrl.getModelFrom(s3Object.getObjectContent()));
+        ctrl.uploadModelToServer(ctrl.getModelFrom(
+                AmazonS3ClientBuilder.standard()
+                        .withRegion(Regions.US_WEST_2)
+                        .build()
+                        .getObject(System.getenv(expectedEnv)
+                                , input.getKey()).getObjectContent()));
 
         ctrl.stopServer();
 
---
+++ sparql-lambda/src/main/java/de/thb/koma/service/sparql/impl/lambda/OntologyKey.java
@@ -0,0 +1,13 @@
+package de.thb.koma.service.sparql.impl.lambda;
+
+public class OntologyKey {
+    private String key;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+}
