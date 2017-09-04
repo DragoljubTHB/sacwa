2bffafe15a3151826178bb52c20be3036c949f05
dado <littlevwing@live.com>
2017-09-04 19:10:40 +0200

    preparing js parser

--- koma-web-static/js/user-controller.js
+++ koma-web-static/js/user-controller.js
@@ -1,118 +1,129 @@
-var userController =  {
-  data: {
-    auth0Lock: null,
-    config: null
-  },
-  uiElements: {
-    loginButton: null,
-    logoutButton: null,
-    profileButton: null,
-    profileNameLabel: null,
-    profileImage: null,
-    sparqlNativeQueryButton: null
-  },
-  init: function(config) {
-    var that = this;
+var userController = {
+    data: {
+        auth0Lock: null,
+        config: null
+    },
+    uiElements: {
+        loginButton: null,
+        logoutButton: null,
+        profileButton: null,
+        profileNameLabel: null,
+        profileImage: null,
+        sparqlNativeQueryButton: null
+    },
+    init: function (config) {
+        var that = this;
 
-    this.uiElements.loginButton = $('#auth0-login');
-    this.uiElements.logoutButton = $('#auth0-logout');
-    this.uiElements.profileButton = $('#user-profile');
-    this.uiElements.profileNameLabel = $('#profilename');
-    this.uiElements.profileImage = $('#profilepicture');
-    this.uiElements.sparqlNativeQueryButton = $('#sparqlNativeQueryButton');
+        this.uiElements.loginButton = $('#auth0-login');
+        this.uiElements.logoutButton = $('#auth0-logout');
+        this.uiElements.profileButton = $('#user-profile');
+        this.uiElements.profileNameLabel = $('#profilename');
+        this.uiElements.profileImage = $('#profilepicture');
+        this.uiElements.sparqlNativeQueryButton = $('#sparqlNativeQueryButton');
 
-    this.data.config = config;
-    this.data.auth0Lock = new Auth0Lock(config.auth0.clientId, config.auth0.domain);
+        this.data.config = config;
+        this.data.auth0Lock = new Auth0Lock(config.auth0.clientId, config.auth0.domain);
 
-    var idToken = localStorage.getItem('userToken');
+        var idToken = localStorage.getItem('userToken');
 
-    if (idToken) {
-      this.configureAuthenticatedRequests();
-      this.data.auth0Lock.getProfile(idToken, function(err, profile) {
-        if (err) {
-          return alert('There was an error getting the profile: ' + err.message);
+        if (idToken) {
+            this.configureAuthenticatedRequests();
+            this.data.auth0Lock.getProfile(idToken, function (err, profile) {
+                if (err) {
+                    return alert('There was an error getting the profile: ' + err.message);
+                }
+                that.showUserAuthenticationDetails(profile);
+            });
         }
-        that.showUserAuthenticationDetails(profile);
-      });
-    }
 
-    this.wireEvents();
-  },
-  configureAuthenticatedRequests: function() {
-    $.ajaxSetup({
-      'beforeSend': function(xhr) {
-          xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('userToken'));
-      }
-    });
-  },
-  showUserAuthenticationDetails: function(profile) {
-    var showAuthenticationElements = !!profile;
-
-    if (showAuthenticationElements) {
-      this.uiElements.profileNameLabel.text(profile.nickname);
-      this.uiElements.profileImage.attr('src', profile.picture);
-    }
-
-    this.uiElements.loginButton.toggle(!showAuthenticationElements);
-    this.uiElements.logoutButton.toggle(showAuthenticationElements);
-    this.uiElements.profileButton.toggle(showAuthenticationElements);
-  },
-  wireEvents: function() {
-    var that = this;
-
-    this.uiElements.loginButton.click(function(e) {
-      var params = {
-        authParams: {
-          scope: 'openid email user_metadata picture'
-        }
-      };
-
-      that.data.auth0Lock.show(params, function(err, profile, token) {
-        if (err) {
-          // Error callback
-          alert('There was an error');
-        } else {
-          // Save the JWT token.
-          localStorage.setItem('userToken', token);
-          that.configureAuthenticatedRequests();
-          that.showUserAuthenticationDetails(profile);
-        }
-      });
-    });
-
-    this.uiElements.logoutButton.click(function(e) {
-      localStorage.removeItem('userToken');
-
-      that.uiElements.logoutButton.hide();
-      that.uiElements.profileButton.hide();
-      that.uiElements.loginButton.show();
-    });
-    this.uiElements.profileButton.click(function(e) {
-      var url = that.data.config.apiBaseUrl + '/userprofile';
-
-      $.get(url, function(data, status) {
-        console.log(JSON.stringify(data));
-        alert(JSON.stringify(data));
-        //$('#user-profile-raw-json').text(JSON.stringify(data, null, 2));
-        //$('#user-profile-modal').modal();
-      })
-    });
-    this.uiElements.sparqlNativeQueryButton.click(function (e) {
-        var url = that.data.config.apiBaseUrl + '/sparql';
-        var query = $('#sparqlNativeQuery').val();
+        this.wireEvents();
+    },
+    configureAuthenticatedRequests: function () {
         $.ajaxSetup({
-           'beforeSend': function (xhr) {
-               xhr.setRequestHeader('query', query)
-           }
+            'beforeSend': function (xhr) {
+                xhr.setRequestHeader('Authorization', 'Bearer ' + localStorage.getItem('userToken'));
+            }
         });
-        $.get(url, function (data, status) {
-            console.log(data);
-            alert(JSON.stringify(data, null, 2));
-        })
-    });
-  }
-};
+    },
+    showUserAuthenticationDetails: function (profile) {
+        var showAuthenticationElements = !!profile;
 
+        if (showAuthenticationElements) {
+            this.uiElements.profileNameLabel.text(profile.nickname);
+            this.uiElements.profileImage.attr('src', profile.picture);
+        }
+
+        this.uiElements.loginButton.toggle(!showAuthenticationElements);
+        this.uiElements.logoutButton.toggle(showAuthenticationElements);
+        this.uiElements.profileButton.toggle(showAuthenticationElements);
+    },
+    wireEvents: function () {
+        var that = this;
+
+        this.uiElements.loginButton.click(function (e) {
+            var params = {
+                authParams: {
+                    scope: 'openid email user_metadata picture'
+                }
+            };
+
+            that.data.auth0Lock.show(params, function (err, profile, token) {
+                if (err) {
+                    // Error callback
+                    alert('There was an error');
+                } else {
+                    // Save the JWT token.
+                    localStorage.setItem('userToken', token);
+                    that.configureAuthenticatedRequests();
+                    that.showUserAuthenticationDetails(profile);
+                }
+            });
+        });
+
+        this.uiElements.logoutButton.click(function (e) {
+            localStorage.removeItem('userToken');
+
+            that.uiElements.logoutButton.hide();
+            that.uiElements.profileButton.hide();
+            that.uiElements.loginButton.show();
+        });
+        this.uiElements.profileButton.click(function (e) {
+            var url = that.data.config.apiBaseUrl + '/userprofile';
+
+            $.get(url, function (data, status) {
+                console.log(JSON.stringify(data));
+                alert(JSON.stringify(data));
+                //$('#user-profile-raw-json').text(JSON.stringify(data, null, 2));
+                //$('#user-profile-modal').modal();
+            })
+        });
+        this.uiElements.sparqlNativeQueryButton.click(function (e) {
+            var url = that.data.config.apiBaseUrl + '/sparql';
+            var ontology = $('#ontologyUrl').val();
+            var query = $('#sparqlNativeQuery').val();
+            $.ajaxSetup({
+                'beforeSend': function (xhr) {
+                    xhr.setRequestHeader('ontology', ontology.toString());
+                    xhr.setRequestHeader('query', query.toString())
+                }
+            });
+            $.get(url, function (data, status) {
+                console.log(data);
+                alert(JSON.stringify(data, null, 2));
+            })
+        });
+    }
+};
+/*
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+                            PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+                            PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+                            SELECT ?comp WHERE {
+                            ?comp rdf:type owl:Class .
+                            }
+                            ORDER BY ?label
+                        </textarea>
+ */
 
 /*
     var SparqlParser = require('sparqljs').Parser;
