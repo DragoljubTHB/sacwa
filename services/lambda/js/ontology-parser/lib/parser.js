bb61a128db4c1fcf5c255feaf36c320e12fb50f5
dado <littlevwing@live.com>
2017-09-05 22:33:30 +0200

    impl parser

--- services/lambda/js/ontology-parser/lib/parser.js
+++ services/lambda/js/ontology-parser/lib/parser.js
@@ -1,9 +1,17 @@
 'use strict';
+var N3 = require('n3');
 
 
-
-function parseBy(individual, fromResource) {
-    return fromResource;
+function parseBy(individual, data) {
+    var parser = new N3.Parser();
+    var found;
+    parser.parse(data, function (error, triple, prefixes) {
+        if(triple){
+            found = N3.Util.getTriples(individual, null, null, null);
+        }
+    });
+    console.log(found);
+    return JSON.stringify(found);
 };
 module.exports = {
     parseBy: parseBy
